2a1e238884b2c839709ec2900105231c828a6f7f
AlexCapatina <105219269+AlexCapatina@users.noreply.github.com>
2022-06-23 16:04:52 -0400

    added mongoDB link, added books to database, added Add, edit and delete buttons in index.ejs

---
+++ Server/Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/books229";
+exports.RemoteURI = "mongodb+srv://AlexCapatina:5PoxrTsjhaoqnMfz@cluster1.5ddxn.mongodb.net/Books?retryWrites=true&w=majority";
+exports.HostName = "MongoDB Atlas";
+exports.Secret = "someSecret";
+//# sourceMappingURL=db.js.map
---
+++ Server/Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://127.0.0.1/books229";
+export const RemoteURI = "mongodb+srv://AlexCapatina:5PoxrTsjhaoqnMfz@cluster1.5ddxn.mongodb.net/Books?retryWrites=true&w=majority"
+export const HostName = "MongoDB Atlas";
+export const Secret = "someSecret";
